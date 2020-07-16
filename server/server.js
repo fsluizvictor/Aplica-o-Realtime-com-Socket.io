804baeddedd6e41c3a9521b5f38205289dad7ca3
luiz victor <fs.luizvictor@gmail.com>
2020-07-16 01:28:31 -0300

    montando server com o socket.io

--- server/server.js
+++ server/server.js
@@ -10,6 +10,17 @@ const io = socket(server)
 const SERVER_HOST = 'localhost'
 const SERVER_PORT = 8080
 
+io.on('connect', socket => {
+    console.log('[IO] Connection => Server has a new connection')
+    socket.on('chat.message', data => {
+        console.log('[SOCKET] Chat.message => ', data)
+    })
+    socket.on('disconnect', () => {
+        console.log('[SOCKET] Disconnect => A connection was disconnected')
+    })
+})
+
+//parâmetros passados : endereço do servidoe, porta do servidor, função callback
 server.listen(SERVER_PORT, SERVER_HOST, () => {
     console.log(`[HTTP] Listen => Server is running at http://${SERVER_HOST}:${SERVER_PORT}`)
 })
